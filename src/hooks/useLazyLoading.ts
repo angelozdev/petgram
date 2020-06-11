db4c27cdc935644d3790868db66edd5298faabb5
Angelo Zambrano <angelozam17@gmail.com>
2020-06-11 18:27:45 -0500

    Support for intersection-observer

--- src/hooks/useLazyLoading.ts
+++ src/hooks/useLazyLoading.ts
@@ -1,20 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 export const useLazyLoading = (element) => {
    const [isShow, setIsShow] = useState(false)
    
    useEffect(() => {
-      const observer = new IntersectionObserver((entries) => {
-         const { isIntersecting } = entries[0];
-         if(isIntersecting){
-            setIsShow(true)
-            observer.disconnect()
-         }
-      }, {
-         threshold: .25
+      /* ¿Intersection es compatible? */
+      Promise.resolve(
+         window.IntersectionObserver 
+         ? window.IntersectionObserver
+         /* ¿no? importe esta dependencia*/
+         : import('intersection-observer')
+      )
+      /* La importación se parchea automáticamente */
+      .then(() => {
+         const observer = new IntersectionObserver((entries) => {
+            const { isIntersecting } = entries[0];
+            if(isIntersecting){
+               setIsShow(true)
+               observer.disconnect()
+            }
+         })
+         observer.observe(element.current)
       })
-   
-      observer.observe(element.current)
+      .catch(err => console.error(err))
       
    }, [element])
 
